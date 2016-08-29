5d4a1ef09123aa346602aaf3a2a73d1080ca1bc4
Ivan <hladysh.im@gmail.com>
2016-08-29 09:31:36 +0000

    add delegation

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
 (function() {
  
  
- var buttons = document.getElementsByTagName('button');
+//  var buttons = document.getElementsByTagName('button');
  
+//  var changeColor = function (e) {   
+//  	eventsObj.preventDefault(e);
+ 
+//  	var elem = eventsObj.getTarget(e);
+ 
+// 	if (elem.id === 'day') {
+// 		document.body.className = 'day';
+// 	} else if (elem.id === 'night') {
+// 		document.body.className = 'night';
+// 	} 
+//  };
+ 
+ 
+//  for (var i = 0, len = buttons.length; i < len; i++) {   
+//  	eventsObj.addEvent(buttons[i], 'click',changeColor ); 
+//  	//eventsObj.removeEvent(buttons[i], 'click',changeColor ); 
+//  };
+
+
+
+// Делегуання подій
+
+
  var changeColor = function (e) {   
  	eventsObj.preventDefault(e);
  
- 	var elem = eventsObj.getTarget(e);
+ 	var elem = eventsObj.getTarget(e),
+ 		colorData = elem.getAttribute('data-color'); 
  
-	if (elem.id === 'day') {
-		document.body.className = 'day';
-	} else if (elem.id === 'night') {
-		document.body.className = 'night';
-	} 
+ 	if ( colorData ) {
+ 
+ 		if (e.type === 'click') {
+ 			document.body.className = '';
+ 
+ 		} else if (e.type === 'mouseover') { 
+ 			document.body.className = colorData;
+ 		}
+ 	}	 
  };
  
  
- for (var i = 0, len = buttons.length; i < len; i++) {   
- 	eventsObj.addEvent(buttons[i], 'click',changeColor ); 
- 	//eventsObj.removeEvent(buttons[i], 'click',changeColor ); 
- };
+ eventsObj.addEvent(document, 'click', changeColor );
+ eventsObj.addEvent(document, 'mouseover', changeColor );
  
  
  
